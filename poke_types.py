04ab47974b8d6d4c94bf7ef7a35bf83d9ffc200a
sigmotoa <si.galvismotoa@gmail.com>
2026-09-14 11:26:26 -0500

    [Add]poketypes is the enum for the pokemon types

---
+++ poke_types.py
@@ -0,0 +1,11 @@
+from enum import Enum
+
+class Poke_type(str, Enum):
+    LEAF = "Leaf"
+    FIRE = "Fire"
+    WATER = "Water"
+    BUG = "Bug"
+    GHOST = "Ghost"
+    POISSON = "Poisson"
+    ELECTRIC = "Electric"
+    FAIRY = "Fairy"

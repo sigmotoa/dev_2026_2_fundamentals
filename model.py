0aa1135a4698672d649ab7239945336942f31129
sigmotoa <si.galvismotoa@gmail.com>
2026-09-14 11:25:49 -0500

    [Add]split model up to a new file

---
+++ model.py
@@ -0,0 +1,16 @@
+from pydantic import BaseModel
+from poke_types import Poke_type
+
+class PokemonBase(BaseModel):
+    id:int
+    name:str
+    lvl:int
+    type_1:Poke_type
+    type_2:Poke_type | None = None
+    #types:Poke_type = [2]
+    is_alive:bool
+    have_evolve:bool
+
+class PokemonCatched(BaseModel):
+    name:str
+    type:Poke_type
